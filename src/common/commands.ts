cf3c761cf6472e590a1948663c6c009921cbb339
Andrei Karev <an.carev4@gmail.com>
2023-01-22 13:10:31 +0300

    feat: implement commands list

---
+++ src/common/commands.ts
@@ -0,0 +1,11 @@
+export enum commands {
+  UP = 'mouse_up',
+  RIGHT = 'mouse_right',
+  DOWN = 'mouse_down',
+  LEFT = 'mouse_left',
+  COORDINATES = 'mouse_position',
+  DRAW_CIRCLE = 'draw_circle',
+  DRAW_RECTANGLE = 'draw_rectangle',
+  DRAW_SQUARE = 'draw_square',
+  PRINT_SCREENSHOT = 'prnt_scrn',
+}
